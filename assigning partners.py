f58c164faf77d888a511a10e75d599da3cba2804
emmawigglesworth <56494865+emmawigglesworth@users.noreply.github.com>
2019-10-16 22:58:36 -0400

    Add files via upload

---
+++ assigning partners.py
@@ -0,0 +1,26 @@
+def test(first, second, nameNum):
+    for x in range(nameNum):
+        partner = second[x]
+        for y in range(x+1,nameNum-1):
+            if first[y]==(partner):
+                index = y
+                if second[index]!= first[x]:
+                    return False   
+    return True
+
+def main():
+
+    nameNum = int(input())
+    first = input()
+    first = first.split()
+    second = input()
+    second = second.split()
+
+    result = test(first, second, nameNum)
+
+    if result:
+        print("good")
+    else: 
+        print("bad")
+
+main()
